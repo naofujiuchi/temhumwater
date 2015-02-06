88ca461c2aaf069593894e6eadcdee236e91946a
naofujiuchi <naofujiuchi@gmail.com>
2015-02-06 14:54:17 +0900

    bekkan

--- ar_temhumwater/ar_temhumwater.ino
+++ ar_temhumwater/ar_temhumwater.ino
@@ -2,174 +2,332 @@
 //2: Humidity sensor for humidifying
 //3: Water level sensor for humidifying
 
-#include "DHT.h"
+import processing.serial.*;  // Import serial library
 
-#define LEVELPIN1 A0
-#define DHTPIN A2     // what pin we're connected to humidity sensor
-#define LEVELPIN2 A4
-#define LEVELPIN3 A5
+PFont font;  // Instance of font class
+PImage imgA;  // Instance of image class
+PImage imgB;
+boolean flag;
+Serial myPort;  // Instance of serial class
 
-// Uncomment whatever type you're using!
-//#define DHTTYPE DHT11   // DHT 11 
-#define DHTTYPE DHT22   // DHT 22  (AM2302)
-//#define DHTTYPE DHT21   // DHT 21 (AM2301)
-
-// Connect pin 1 (on the left) of the sensor to +5V
-// Connect pin 2 of the sensor to whatever your DHTPIN is
-// Connect pin 4 (on the right) of the sensor to GROUND
-// Connect a 10K resistor from pin 2 (data) to pin 1 (power) of the sensor
-
-DHT dht(DHTPIN, DHTTYPE);
-
+int s;
+int m;
+int h;
+int period;
 float hum;
-float tem;
-int outPin[4];
-int levelval[4];
-int level[4];
-int inByte[4];
-int highlow[4];
+float temp;
+String t;
+int[] workingtime = new int[4];
+int[] press = new int[4];
+int[] level = new int[4];
+int[] inByte = new int[4];
+int[] out = new int[4];
+int[] serial = new int[8];
+String[] state = new String[4];
+String[] onoff = new String[4];
+PImage[] img = new PImage[4];
 
 void setup() {
-  Serial.begin(9600); 
-  for(int i = 1; i <= 3; i++){
-    outPin[i] = 10 + i;
-    pinMode(outPin[i], OUTPUT);
-    levelval[i] = 0;
+  int i;
+  size(1000, 400);
+  frameRate(1);
+  font = loadFont("Calibri-24.vlw");
+  textFont(font);
+  textAlign(RIGHT);
+  myPort = new Serial(this, "/dev/cu.usbmodem1411", 9600);
+  myPort.buffer(10);
+  imgA = loadImage("playgreen.png");
+  imgB = loadImage("pausegreen.png");
+  flag = false;
+  for (i = 1; i <= 3; i++) {
+    workingtime[i] = 0;
+    press[i] = 0;
     level[i] = 0;
     inByte[i] = 0;
-    highlow[i] = 0;
+    out[i] = 0;
+    serial[i] = 0;
+    state[i] = "Stop";
+    onoff[i] = "default";
+    img[i] = imgB;
   }
-  dht.begin();
 }
 
-void loop(){
-  // Reading temperature or humidity takes about 250 milliseconds!
-  // Sensor readings may also be up to 2 s  econds 'old' (its a very slow sensor)
-//  float hum = dht.readHumidity();  // !!Unless connecting to DHT sensor, errors would arise!!
-//  float tem = dht.readTemperature();
-  // check if returns are valid, if they are NaN (not a number) then something went wrong!
-//  checkDHT(tem, hum);
-  for(int i = 1; i <= 3; i++){
-    levelval[i] = getLevelval(i);
-    level[i] = getLevel(i, levelval[i]);
+void draw() {
+  int i;
+  background(100);
+  getTime();
+  if (myPort.available() == 7) {
+    readSerial();
+    myPort.clear();  //clear serial buffer
+    for (i = 1; i <= 3; i++) {
+      onoff[i] = onoffTime(i, h, m);
+      out[i] = getOut(i);
+      myPort.write(out[i]);
+      img[i] = getPicture(i, state[i]);
+    }
   }
-/*
-  Serial.print("1: ");
-  Serial.print(levelval[1]);
-  Serial.print(", 3: ");
-  Serial.println(levelval[3]);
+  display();
+  if(period == 20){
+    timeReset();
+  }
   delay(1000);
-*/
-  if(Serial.available() == 3){
-    for(int i = 1; i <= 3; i++){
-      inByte[i] = getinByte();
-    }
-    for(int i = 1; i <= 3; i++){
-      highlow[i] = getHighlow(inByte[i]);
-      digitalWrite(outPin[i], highlow[i]);
-    }
-    Serial.flush();
-    // Serial write
-    Serial.write(inByte[1]);
-    Serial.write(inByte[2]);
-    Serial.write(inByte[3]);
-    Serial.write(level[1]);
-    Serial.write(level[3]);
-    Serial.write(hum);
-    Serial.write(tem);
+}
+
+void timeReset(){
+  int i;
+  period = 0;
+  workingtime[3] = 0;
+}
+
+void getTime() {
+  h = int(hour());
+  m = int(minute());
+  s = int(second());
+  t = h + ":" + nf(m, 2) + ":" + nf(s, 2);
+}
+
+void readSerial() {
+  int i;
+  for(i = 1; i <= 7; i++){
+    serial[i] = myPort.read();
+//    println(i + ": " + serial[i]);
   }
-
+  inByte[1] = serial[1];
+  inByte[2] = serial[2];
+  inByte[3] = serial[3];
+  level[1] = serial[4];
+  level[3] = serial[5];
+  hum = serial[6];
+  temp = serial[7];
 }
 
-int getinByte(){
-  int _inByte = Serial.read();
-  return _inByte;
-}
-
-int getHighlow(int _inByte){
-  switch(_inByte){
-    case 0:
-      return HIGH;
+String onoffTime(int _i, int _h, int _m) {
+  switch(_i) {
     case 1:
-      return LOW;
-    case 2:
-      return LOW;
-    case 3:
-      return HIGH;
-    case 4:
-      return LOW;
-    default:
-      return LOW;
-  }
-}
-
-int getLevelval(int _i){
-  int _levelval;
-  switch(_i){
-    case 1:
-      _levelval = analogRead(LEVELPIN1);
-      break;
-    case 3:
-//      _levelval = determineLevel();
-      _levelval = analogRead(LEVELPIN2);
-      break;
-    default:
-      _levelval = 0;
-      break;
-  }
-  return _levelval;
-}
-
-/*
-int determineLevel(){
-  int _levelvalA4;
-  int _levelvalA5;
-  _levelvalA4 = analogRead(LEVELPIN2);
-  _levelvalA5 = analogRead(LEVELPIN3);
-//  return _levelvalA4;
-  if((_levelvalA4 == 0) && (_levelvalA5 == 0)){
-    return 0;
-  }else if((_levelvalA4 > 0) && (_levelvalA5 == 0)){
-    return 1;
-  }else if((_levelvalA4 > 0) && (_levelvalA5 > 0)){
-    return 2;
-  }else{
-    return 0;
-  }
-}
-*/
-
-/*
-void checkDHT(float _tem, float _hum){
-  if(isnan(_tem) || isnan(_hum)){
-    Serial.println("Failed to read from DHT");
-    delay(10000);
-  }
-}
-*/
-int getLevel(int _i, int _levelval){
-  switch(_i){
-    case 1:
-    case 3:
-      if(_levelval > 100){
-        return 1;
+      if ((_h == 5) && (_m < 20)) {
+        return "OnTime";
       }else{
-        return 0;
+        return "OffTime";
       }
-/*
-    case 3:  // fluid level sensor (eTape, MILONE Technologies); if level is 5 cm then output 400, and level is 10 cm then output 450 when connected input voltage 5 V and 1 kΩ resistance
-      return _levelval;
-*/
-/*
-      if(_levelval <= 400){
-        return 0;
-      }else if((_levelval > 400) && (_levelval <= 450)){
-        return 1;
-      }else if(_levelval > 450){
-        return 2;
-      }
-*/
+    case 2:
+      return "OnTime";
+    case 3:
+      return "OnTime";
     default:
-      return 0;
+      println("Error in onoffTime");
+      return "NoMatch";
+  }
+}
+
+int getOut(int _i) {
+  int _out;
+  switch(_i){
+    case 1:
+      _out = outLevel(_i, inByte[_i], level[_i], state[_i], onoff[_i], workingtime[_i]);
+      break;
+    case 2:
+      _out = outHum(_i, inByte[_i], hum, state[_i], onoff[_i]);
+      break;
+    case 3:
+      _out = outLevel2(_i, inByte[_i], level[_i], state[_i], onoff[_i], workingtime[_i]);
+      break;
+    default:
+      println("Error in getout");
+      _out = 4;
+      break;
+  }
+  return _out;
+}
+
+int outLevel(int _i, int _inByte, int _level, String _state, String _onoff, int _workingtime) {
+  boolean _TFlevel = judgeLevel(_level);
+  boolean _TFstate = judgeState(_state);
+  boolean _TFonoff = judgeOnoff(_onoff);
+  if(_TFlevel && _TFstate && _TFonoff){
+    return 3;
+  }
+  else if (_inByte == 0) {
+    return 1;
+  }
+  else {
+    return 1;
+  }
+}
+
+int outLevel2(int _i, int _inByte, int _level, String _state, String _onoff, int _workingtime) {
+  int _outlevel;
+  boolean _TFlevel = judgeLevel(_level);
+  boolean _TFstate = judgeState(_state);
+  boolean _TFonoff = judgeOnoff(_onoff);
+  boolean _TFworkingtime = judgeWorkingtime(_workingtime);
+  switch(_inByte){
+    case 0:  // initial
+      workingtime[_i] = 0;
+      _outlevel = 1;
+      break;
+    case 1:  // period will not increase
+      if (_TFlevel && _TFstate && _TFonoff && _TFworkingtime) { 
+        period++;
+        workingtime[_i]++;
+        _outlevel = 3;
+      }else if(!_TFstate){
+        _outlevel = 1;
+      }else{
+        _outlevel = 2;
+      }
+      break;
+    case 2:
+      if (_TFlevel && _TFstate && _TFonoff && _TFworkingtime) {
+        if(period == 0){
+          workingtime[_i]++;
+          _outlevel = 3;
+        }else{
+          _outlevel = 2;
+        }
+      }else if(!_TFstate){
+        _outlevel = 1;
+      }else{
+        _outlevel = 2;
+      }
+      period++;
+      break;
+    case 3:
+      if(!_TFworkingtime){
+        _outlevel = 4;
+      }else if(_TFlevel && _TFstate && _TFonoff) {
+        period++;
+        workingtime[_i]++;
+        _outlevel = 3;
+      }else{
+        period++;
+        _outlevel = 2;
+      }
+      break;  
+    case 4:  // keep stop state
+      if(!_TFstate){
+        timeReset();
+        _outlevel = 1;
+      }else{
+        _outlevel = 4;
+      }
+      break;
+    default:
+      _outlevel = 4;
+  }
+  println("WT: " + workingtime[_i] + ", period: " + period);
+  return _outlevel;
+}
+
+boolean judgeLevel(int _level){
+  if(_level == 0){
+    return true;
+  }else{
+    return false;
+  }
+}
+
+boolean judgeState(String _state){
+  if(_state == "Running"){
+    return true;
+  }else{
+    return false;
+  }
+}
+
+boolean judgeOnoff(String _onoff){
+  if(_onoff == "OnTime"){
+    return true;
+  }else{
+    return false;
+  }
+}
+
+boolean judgeWorkingtime(int _workingtime){
+  if(_workingtime < 10){
+    return true;
+  }else{
+    return false;
+  }
+}
+
+int outHum(int _i, int _inByte, float _hum, String _state, String _onoff) {
+  if ((_state == "Running") && (_hum < 90) && (_onoff == "OnTime")) {
+    return 3;
+  }
+  else if (_inByte == 0) {
+    return 1;
+  }
+  else {
+    return 1;
+  }
+}
+
+PImage getPicture(int _i, String _state) {
+  boolean _TFstate = judgeState(_state);
+  if(_TFstate){
+    return imgA;
+  }else{
+    return imgB;
+  }
+}
+
+//ProcessingをRunしたときに出てくるウィンドウをクリックすると
+void mousePressed() {
+  press[0]++;
+  if (press[0] == 1) {
+    myPort.clear();
+    myPort.write(0);
+    myPort.write(0);
+    myPort.write(0);
+    println("Serial initialized");
+  }
+  if (mouseX >= 40 && mouseX <= 220 && mouseY >= 180 && mouseY <= 360) {
+    countPress(1);
+  }
+  if (mouseX >= 290 && mouseX <= 470 && mouseY >= 180 && mouseY <= 360) {
+    countPress(2);
+  }
+  if (mouseX >= 540 && mouseX <= 720 && mouseY >= 180 && mouseY <= 360) {
+    countPress(3);
+  }
+}
+
+void countPress(int _i) {
+  press[_i]++;
+  if (press[_i] % 2 == 1) {
+    state[_i] = "Running";
+  }
+  else {
+    state[_i] = "Stop";
+  }
+}
+
+void display() {
+  int i;
+  text("Autowater", 180, 20);
+  text("Humidifier1", 430, 20);
+  text("Humidifier2", 680, 20);
+  text(t, 295, 20);
+  text("level", 275, 50);
+  text(level[1], 135, 50);
+  text(level[3], 635, 50);
+  text("humidity", 295, 70);
+  text(hum, 410, 70);
+  text("temperature", 315, 90);
+  text(temp, 410, 90);
+  text("state", 275, 110);
+  text(state[1], 160, 110);
+  text(state[2], 410, 110);
+  text(state[3], 660, 110);
+  text("onoffTime", 300, 130);
+  text(onoff[1], 160, 130);
+  text(onoff[2], 410, 130);
+  text(onoff[3], 660, 130);
+  text("inByte", 280, 150);
+  text(inByte[1], 135, 150);
+  text(inByte[2], 390, 150);
+  text(inByte[3], 645, 150);
+  for(i = 1; i <=3; i++){
+    image(img[i], 250 * (i - 1), 140);
   }
 }
